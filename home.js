630ea3c39053672345255042e680524a0a79e65c
AmanMishra11 <amanmishralm10@gmail.com>
2025-01-31 23:26:57 +0530

    fixed an error in home page

--- home.js
+++ home.js
@@ -88,7 +88,7 @@ const projects = [
       title: "Dad Jokes Generator",
       description: "A web application that delivers dad jokes on-demand with features like favorites, sharing, and theme customization, built with HTML, CSS, and JavaScript, inspired by Google AI's design aesthetics. ",
       image: "./assets/dadjoke.png",
-      liveLink: "https://amanmishra11.github.io/js-2/image.html",
+      liveLink: "https://amanmishra11.github.io/js-2/dadjoke.html",
       codeLink: "https://github.com/AmanMishra11/js-2",
     }
   ];
